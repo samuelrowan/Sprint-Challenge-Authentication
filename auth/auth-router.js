5dcc46825597bed90d476e5e8ec613a1e1d012a9
Samuel Rowan <samuel@samuelrowan.com>
2020-05-17 18:26:29 -0500

    Getting started

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,51 @@
 const router = require('express').Router();
 
 router.post('/register', (req, res) => {
-  // implement registration
+  try {
+		const { username } = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		res.status(201).json(await Users.add(req.body))
+	} catch(err) {
+		next(err)
+	}
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const authError = {
+		message: "Invalid Credentials",
+	}
+
+	try {
+		const user = await Users.findBy({ username: req.body.username }).first()
+		if (!user) {
+			return res.status(401).json(authError)
+		}
+
+		const passwordValid = await bcrypt.compare(req.body.password, user.password)
+		if (!passwordValid) {
+			return res.status(401).json(authError)
+		}
+
+		const tokenPayload = {
+			userId: user.id,
+			userRole: "admin",
+		}
+
+		res.cookie("token", jwt.sign(tokenPayload, process.env.JWT_SECRET))
+
+		res.json({
+			message: `Welcome ${user.username}!`,
+		})
+	} catch(err) {
+		next(err)
+	}
 });
 
 module.exports = router;
